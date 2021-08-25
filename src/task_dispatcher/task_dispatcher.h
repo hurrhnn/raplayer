c00369d0117aff4dc08f2c9d4be783f5786891f0
hurrhnn <20sunrin022@sunrint.hs.kr>
2021-08-25 18:39:02 +0900

    Added `task_dispatcher` to use data processed by `task_scheduler`.

---
+++ src/task_dispatcher/task_dispatcher.h
@@ -0,0 +1,28 @@
+/*
+ raplayer is a cross-platform remote audio player, written from the scratch.
+ This file is part of raplayer.
+
+ Copyright (C) 2021 Rhnn Hur (hurrhnn)
+
+    raplayer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPUSSTREAMER_SERVER_TASK_DISPATCHER_H
+#define OPUSSTREAMER_SERVER_TASK_DISPATCHER_H
+
+#include "../task_scheduler/task_scheduler.h"
+#include "../task_scheduler/task_queue/task_queue.h"
+
+Task recvfrom_queue(TaskQueue *recv_queue);
+#endif
