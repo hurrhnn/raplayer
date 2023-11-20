65bdf918c0e7581a2730968d77dc7b01c96fa662
hurrhnn <20sunrin022@sunrint.sen.hs.kr>
2023-11-21 06:24:04 +0900

    raplayer: integrated utility stuffs
    Signed-off-by: hurrhnn <20sunrin022@sunrint.sen.hs.kr>

---
+++ include/raplayer/utils.h
@@ -0,0 +1,26 @@
+#ifndef RAPLAYER_UTILS_H
+#define RAPLAYER_UTILS_H
+
+#include <raplayer/config.h>
+#include <stdlib.h>
+
+#ifdef __ANDROID__
+#ifndef RAPLAYER_ANDROID_WORKAROUND
+#define RAPLAYER_ANDROID_WORKAROUND
+
+#include <pthread.h>
+#define pthread_cancel(h) pthread_kill(h, 0)
+
+#define PTHREAD_CANCEL_ENABLE 0
+#define PTHREAD_CANCEL_ASYNCHRONOUS 0
+#define pthread_setcanceltype(foo, bar) \
+        while(foo);                     \
+        while(bar);                     \
+
+#define pthread_setcancelstate(foo, bar) pthread_setcanceltype(foo, bar)
+#endif
+#endif /* __ANDROID__ */
+
+uint64_t provide_20ms_opus_offset_calculator(unsigned char c_bits[MAX_DATA_SIZE], unsigned char** result);
+
+#endif //RAPLAYER_UTILS_H
---
+++ src/utils.c
@@ -0,0 +1,21 @@
+#include <raplayer/utils.h>
+
+uint64_t provide_20ms_opus_offset_calculator(unsigned char c_bits[MAX_DATA_SIZE], unsigned char** result) {
+    long unsigned int idx = 0;
+    for (long unsigned int i = 0; i < MAX_DATA_SIZE; i++) { // 'OPUS' indicates for Start of opus stream.
+        if (c_bits[i] == 'O' && c_bits[i + 1] == 'P' && c_bits[i + 2] == 'U' && c_bits[i + 3] == 'S') {
+            idx = i - 1;
+            break;
+        }
+    }
+
+    char str_nbBytes[DWORD];
+    for (long unsigned int i = 0; i <= idx + 1; i++) {
+        if (i == (idx + 1))
+            str_nbBytes[i] = '\0';
+        else
+            str_nbBytes[i] = (char) c_bits[i];
+    }
+    *result = c_bits + idx + sizeof(OPUS_FLAG);
+    return strtol(str_nbBytes, NULL, 10);
+}
