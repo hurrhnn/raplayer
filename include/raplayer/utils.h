068ea24ac37e71bae933a1ce6c1f1cd46958991c
hurrhnn <20sunrin022@sunrint.sen.hs.kr>
2023-12-08 06:38:51 +0900

    utils: imported macros to check system endianness
    Signed-off-by: hurrhnn <20sunrin022@sunrint.sen.hs.kr>

--- include/raplayer/utils.h
+++ include/raplayer/utils.h
@@ -6,7 +6,24 @@
 #include <sys/socket.h>
 #include <raplayer/config.h>
 
-#include <stdio.h>
+#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN || \
+    defined(__BIG_ENDIAN__) || \
+    defined(__ARMEB__) || \
+    defined(__THUMBEB__) || \
+    defined(__AARCH64EB__) || \
+    defined(_MIBSEB) || defined(__MIBSEB) || defined(__MIBSEB__)
+#define RA_SYSTEM_IS_BIG_ENDIAN
+#elif defined(__BYTE_ORDER) && __BYTE_ORDER == __LITTLE_ENDIAN || \
+    defined(__LITTLE_ENDIAN__) || \
+    defined(__ARMEL__) || \
+    defined(__THUMBEL__) || \
+    defined(__AARCH64EL__) || \
+    defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__) || \
+    defined(BUFEP_MS_WINDOWS)
+#define RA_SYSTEM_IS_LITTLE_ENDIAN
+#else
+#error "Couldn't decide the system endianness."
+#endif
 
 #ifdef __ANDROID__
 #ifndef RAPLAYER_ANDROID_WORKAROUND
