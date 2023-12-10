0582bcfddafca758e556d6ce8ee2240dddd35701
hurrhnn <20sunrin022@sunrint.sen.hs.kr>
2023-12-10 18:55:39 +0900

    dispatcher: initially implemented packet dispatcher
    Signed-off-by: hurrhnn <20sunrin022@sunrint.sen.hs.kr>

---
+++ include/raplayer/dispatcher.h
@@ -0,0 +1,6 @@
+#ifndef RAPLAYER_DISPATCHER_H
+#define RAPLAYER_DISPATCHER_H
+
+void *dispatch_packet(void *p_ra_packet_dispatcher_args);
+
+#endif //RAPLAYER_DISPATCHER_H
---
+++ src/dispatcher.c
@@ -0,0 +1,41 @@
+#include <raplayer/dispatcher.h>
+#include <raplayer/node.h>
+#include <raplayer/task_queue.h>
+
+void *dispatch_packet(void *p_ra_packet_dispatcher_args) {
+    ra_node_t *node = p_ra_packet_dispatcher_args;
+    while (!(node->status & RA_NODE_CONNECTION_EXHAUSTED)) {
+        ra_task_t *task = retrieve_task(node->recv_queue);
+        if(memcmp(task->data, RA_CTL_HEADER, DWORD) == 0) {
+            uint8_t type = -1;
+            memcpy(&type, task->data + DWORD, BYTE);
+            switch (type) {
+                case 0x00:
+                    if(!(node->status & RA_NODE_CONNECTED)) {
+                        void* heartbeat_msg = malloc(DWORD + BYTE);
+                        memcpy(heartbeat_msg, RA_CTL_HEADER, DWORD);
+                        memcpy(heartbeat_msg + DWORD, "\x00", BYTE);
+
+                        sendto((int) node->local_sock->fd, heartbeat_msg, DWORD + BYTE, 0,
+                               (struct sockaddr *) &node->remote_sock->addr,
+                               sizeof(struct sockaddr_in));
+                        node->status |= RA_NODE_CONNECTED;
+                    }
+                    node->status |= RA_NODE_HEARTBEAT_RECEIVED;
+                    break;
+                case 0x01:
+                    memcpy(&node->channels, (task->data + DWORD), WORD);
+                    memcpy(&node->sample_rate, (task->data + DWORD + WORD), DWORD);
+                    memcpy(&node->bit_per_sample, (task->data + DWORD + WORD + DWORD), WORD);
+                    break;
+                case 0x02:
+                    memcpy(&node->crypto_context, (task->data + DWORD), sizeof(struct chacha20_context));
+                default:
+                    break;
+            }
+            remove_task(task);
+        } else
+            append_task(node->frame_queue, task);
+    }
+    return NULL;
+}
