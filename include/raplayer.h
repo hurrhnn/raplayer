6276d14b463cebac70426e48b5c29f1385e55e9f
hurrhnn <20sunrin022@sunrint.sen.hs.kr>
2023-12-10 18:51:28 +0900

    raplayer: reimplemented raplayer api structure
    Signed-off-by: hurrhnn <20sunrin022@sunrint.sen.hs.kr>

--- include/raplayer.h
+++ include/raplayer.h
@@ -2,36 +2,34 @@
 #define RAPLAYER_RAPLAYER_H
 
 #include <raplayer/utils.h>
-#include <raplayer/client.h>
-#include <raplayer/server.h>
 #include <raplayer/chacha20.h>
 #include <raplayer/task_queue.h>
-#include <raplayer/task_scheduler.h>
+#include <raplayer/scheduler.h>
+#include <raplayer/dispatcher.h>
+
+#define RA_MODE_HOST 0
+#define RA_MODE_PEER 1
 
 typedef struct {
-    ra_client_t *client;
-    ra_server_t *server;
+    uint64_t cnt_fds;
+    struct pollfd *fds;
+
+    uint64_t cnt_local_sock;
+    ra_sock_local_t **local_sock;
+
+    uint64_t cnt_remote_sock;
+    ra_sock_remote_t **remote_sock;
+
+    uint64_t cnt_node;
+    ra_node_t **node;
+
+    uint64_t cnt_spawn;
+    ra_spawn_t **spawn;
 } raplayer_t;
 
 void raplayer_init_context(raplayer_t *raplayer);
 
-uint64_t raplayer_spawn_server(raplayer_t *raplayer, int port, uint8_t *(*frame_callback)(void *user_data),
-                               void *callback_user_data);
-
-uint64_t raplayer_spawn_client(raplayer_t *raplayer, char *address, int port,
-                               void(*frame_callback)(void *frame, int frame_size, void *user_data),
-                               void *callback_user_data);
-
-int32_t raplayer_wait_server(raplayer_t *raplayer, uint64_t idx);
-
-int32_t raplayer_wait_client(raplayer_t *raplayer, uint64_t idx);
-
-bool raplayer_get_server_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type);
-
-bool raplayer_get_client_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type);
-
-void raplayer_set_client_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type);
-
-void raplayer_set_server_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type);
+int32_t raplayer_spawn(raplayer_t *raplayer, bool mode, char *address, int port, void *(*send_callback) (void *user_data),
+                       void *send_cb_user_data, void (*recv_callback) (void *frame, int frame_size, void *user_data), void *recv_cb_user_data);
 
 #endif //RAPLAYER_RAPLAYER_H
--- src/raplayer.c
+++ src/raplayer.c
@@ -1,99 +1,130 @@
+#include <netdb.h>
 #include <raplayer.h>
 
 void raplayer_init_context(raplayer_t *raplayer) {
-    raplayer->client = malloc(sizeof(ra_client_t));
-    raplayer->client->list = malloc(sizeof(*raplayer->client->list));
-    raplayer->client->idx = -1;
+    memset(raplayer, 0x0, sizeof (raplayer_t));
+    ra_packet_scheduler_args_t *scheduler_args = malloc(sizeof (ra_packet_scheduler_args_t));
+    scheduler_args->cnt_fds = &raplayer->cnt_fds;
+    scheduler_args->fds = &raplayer->fds;
 
-    raplayer->server = malloc(sizeof(ra_server_t));
-    raplayer->server->list = malloc(sizeof(*raplayer->server->list));
-    raplayer->server->idx = -1;
+    scheduler_args->cnt_local_sock = &raplayer->cnt_local_sock;
+    scheduler_args->local_sock = &raplayer->local_sock;
+
+    scheduler_args->cnt_remote_sock = &raplayer->cnt_remote_sock;
+    scheduler_args->remote_sock = &raplayer->remote_sock;
+
+    scheduler_args->cnt_node = &raplayer->cnt_node;
+    scheduler_args->node = &raplayer->node;
+
+    scheduler_args->spawn = &raplayer->spawn;
+    scheduler_args->cnt_spawn = &raplayer->cnt_spawn;
+
+    pthread_t packet_scheduler;
+    pthread_create(&packet_scheduler, NULL, schedule_packet, scheduler_args);
 }
 
-uint64_t raplayer_spawn_server(raplayer_t *raplayer, int port, uint8_t *(*frame_callback)(void *user_data),
-                               void *callback_user_data) {
-    uint64_t idx = ++raplayer->server->idx;
-    raplayer->server->list = realloc(raplayer->server->list, sizeof(*raplayer->server->list) * (idx + 2));
-    memset(&(raplayer->server->list[idx]), 0x0, sizeof(*raplayer->server->list));
+int32_t raplayer_spawn(raplayer_t *raplayer, bool mode, char *address, int port, void *(*send_callback) (void *user_data),
+                       void *send_cb_user_data, void (*recv_callback) (void *frame, int frame_size, void *user_data), void *recv_cb_user_data) {
 
-    raplayer->server->list[idx].port = port;
-    raplayer->server->list[idx].frame_callback = frame_callback;
-    raplayer->server->list[idx].callback_user_data = callback_user_data;
-    raplayer->server->list[idx].status = 0;
+    void *before_address = raplayer->fds;
+    ra_realloc(raplayer->fds, sizeof(struct pollfd) * ((raplayer->cnt_fds) + 1));
+    RA_DEBUG_MORE(GRN, "Reallocated poll fds context %p to %p, size: 0x%llX\n",
+                  before_address,
+                  raplayer->fds,
+                  sizeof(struct pollfd) *
+                  ((raplayer->cnt_fds) + 1));
 
-    pthread_create(&raplayer->server->list[idx].thread, NULL, ra_server, raplayer->server);
-    return idx;
-}
+    before_address = raplayer->local_sock;
+    ra_realloc(raplayer->local_sock, sizeof(ra_sock_local_t *) * ((raplayer->cnt_local_sock) + 1));
+    RA_DEBUG_MORE(GRN, "Reallocated local sock context %p to %p, size: 0x%llX\n",
+                  before_address,
+                  raplayer->local_sock,
+                  sizeof(ra_sock_local_t *) *
+                  ((raplayer->cnt_local_sock) + 1));
 
-uint64_t raplayer_spawn_client(raplayer_t *raplayer, char *address, int port,
-                               void(*frame_callback)(void *frame, int frame_size, void *user_data),
-                               void *callback_user_data) {
-    uint64_t idx = ++raplayer->client->idx;
-    raplayer->client->list = realloc(raplayer->client->list, sizeof(*raplayer->client->list) * (idx + 2));
-    memset(&(raplayer->client->list[idx]), 0x0, sizeof(*raplayer->client->list));
+    int sock_fd;
+    if ((sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
+        return RA_SOCKET_CREATION_FAILED;
 
-    raplayer->client->list[idx].address = address;
-    raplayer->client->list[idx].port = port;
-    raplayer->client->list[idx].frame_callback = frame_callback;
-    raplayer->client->list[idx].callback_user_data = callback_user_data;
-    raplayer->client->list[idx].status = 0;
+    raplayer->local_sock[raplayer->cnt_local_sock] = malloc(sizeof(ra_sock_local_t));
+    raplayer->local_sock[raplayer->cnt_local_sock]->fd = sock_fd;
+    raplayer->local_sock[raplayer->cnt_local_sock]->addr.sin_family = AF_INET;
+    raplayer->local_sock[raplayer->cnt_local_sock]->addr.sin_port = htons((uint16_t) port);
+    if(address == NULL)
+        raplayer->local_sock[raplayer->cnt_local_sock]->addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    else {
+        struct hostent *hostent;
+        struct in_addr **addr_list;
 
-    pthread_create(&raplayer->client->list[idx].thread, NULL, ra_client, raplayer->client);
-    return idx;
-}
+        if ((hostent = gethostbyname(address)) == NULL) {
+            return RA_CONNECTION_RESOLVE_FAILED;
+        } else {
+            addr_list = (struct in_addr **) hostent->h_addr_list;
+            char *resolved_address = malloc(0x100);
+            strcpy(resolved_address, inet_ntoa(*addr_list[0]));
 
-int32_t raplayer_wait_server(raplayer_t *raplayer, uint64_t idx) {
-    int32_t server_status;
-    pthread_join(raplayer->server->list[idx].thread, (void **) &server_status);
-
-    return server_status;
-}
-
-int32_t raplayer_wait_client(raplayer_t *raplayer, uint64_t idx) {
-    int32_t client_status;
-    pthread_join(raplayer->client->list[idx].thread, (void **) &client_status);
-
-    return client_status;
-}
-
-bool raplayer_get_server_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type) {
-    return (raplayer->server->list[idx].status & type);
-}
-
-void raplayer_set_server_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type) {
-    switch (type) {
-        case RA_NODE_INITIATED:
-            raplayer->server->list[idx].status = RA_NODE_INITIATED;
-            break;
-        case RA_NODE_CONNECTED:
-            raplayer->server->list[idx].status = (RA_NODE_INITIATED | RA_NODE_CONNECTED);
-            break;
-        case RA_NODE_CONNECTION_EXHAUSTED:
-            raplayer->server->list[idx].status = RA_NODE_CONNECTION_EXHAUSTED;
-            break;
-        case RA_NODE_HEARTBEAT_RECEIVED:
-            raplayer->server->list[idx].status |= RA_NODE_HEARTBEAT_RECEIVED;
-            break;
+            if (!inet_pton(AF_INET, resolved_address, &raplayer->local_sock[raplayer->cnt_local_sock]->addr.sin_addr))
+                return RA_ADDRESS_CONVERSION_FAILED;
+            free(resolved_address);
+        }
     }
-}
 
-bool raplayer_get_client_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type) {
-    return (raplayer->client->list[idx].status & type);
-}
-
-void raplayer_set_client_status(raplayer_t *raplayer, uint64_t idx, ra_node_status_t type) {
-    switch (type) {
-        case RA_NODE_INITIATED:
-            raplayer->client->list[idx].status = RA_NODE_INITIATED;
-            break;
-        case RA_NODE_CONNECTED:
-            raplayer->client->list[idx].status = (RA_NODE_INITIATED | RA_NODE_CONNECTED);
-            break;
-        case RA_NODE_CONNECTION_EXHAUSTED:
-            raplayer->client->list[idx].status = RA_NODE_CONNECTION_EXHAUSTED;
-            break;
-        case RA_NODE_HEARTBEAT_RECEIVED:
-            raplayer->client->list[idx].status |= RA_NODE_HEARTBEAT_RECEIVED;
-            break;
+    if(mode == RA_MODE_HOST) {
+        /* Bind the socket with local address. */
+        if((bind(sock_fd, (struct sockaddr *) &raplayer->local_sock[raplayer->cnt_local_sock]->addr,
+                 sizeof(struct sockaddr_in))) < 0) {
+            return RA_SOCKET_BIND_FAILED;
+        }
     }
+
+    raplayer->fds[raplayer->cnt_fds].fd = sock_fd;
+    raplayer->fds[raplayer->cnt_fds].events = POLLIN;
+
+    raplayer->cnt_local_sock++;
+    raplayer->cnt_fds++;
+
+    if(mode == RA_MODE_PEER) {
+        void* heartbeat_msg = malloc(0x05);
+        memcpy(heartbeat_msg, RA_CTL_HEADER, DWORD);
+        memcpy(heartbeat_msg + DWORD, "\x00", BYTE);
+        sendto(sock_fd, heartbeat_msg, 0x05, 0,
+               (const struct sockaddr *) &raplayer->local_sock[raplayer->cnt_local_sock - 1]->addr,
+                       sizeof(struct sockaddr_in));
+        free(heartbeat_msg);
+    }
+
+    if(send_callback != NULL) {
+        before_address = raplayer->spawn;
+        ra_realloc(raplayer->spawn, sizeof(ra_spawn_t *) * ((raplayer->cnt_spawn) + 1));
+        RA_DEBUG_MORE(GRN, "Reallocated send spawn context %p to %p, size: 0x%llX\n",
+                      before_address,
+                      raplayer->spawn,
+                      sizeof(ra_spawn_t *) *
+                      ((raplayer->cnt_spawn) + 1));
+
+        raplayer->spawn[raplayer->cnt_spawn] = malloc(sizeof(ra_spawn_t));
+        raplayer->spawn[raplayer->cnt_spawn]->type = RA_SPAWN_TYPE_SEND;
+        raplayer->spawn[raplayer->cnt_spawn]->callback.send = send_callback;
+        raplayer->spawn[raplayer->cnt_spawn]->cb_user_data = send_cb_user_data;
+        raplayer->spawn[raplayer->cnt_spawn]->local_sock = raplayer->local_sock[raplayer->cnt_local_sock - 1];
+        raplayer->cnt_spawn++;
+    }
+
+    if(recv_callback != NULL) {
+        before_address = raplayer->spawn;
+        ra_realloc(raplayer->spawn, sizeof(ra_spawn_t *) * ((raplayer->cnt_spawn) + 1));
+        RA_DEBUG_MORE(GRN, "Reallocated recv spawn context %p to %p, size: 0x%llX\n",
+                      before_address,
+                      raplayer->spawn,
+                      sizeof(ra_spawn_t *) *
+                      ((raplayer->cnt_spawn) + 1));
+
+        raplayer->spawn[raplayer->cnt_spawn] = malloc(sizeof(ra_spawn_t));
+        raplayer->spawn[raplayer->cnt_spawn]->type = RA_SPAWN_TYPE_RECV;
+        raplayer->spawn[raplayer->cnt_spawn]->callback.recv = recv_callback;
+        raplayer->spawn[raplayer->cnt_spawn]->cb_user_data = recv_cb_user_data;
+        raplayer->spawn[raplayer->cnt_spawn]->local_sock = raplayer->local_sock[raplayer->cnt_local_sock - 1];
+        raplayer->cnt_spawn++;
+    }
+    return 0;
 }
